e44525a781a6dc99435fe389e9d8f940e8867476
MaxDunsmore <max.dunsmore@my.jcu.edu.au>
2017-08-10 23:21:05 +1000

    Prac 1
    Problems with DebugOne1

---
+++ MovieQuote.java
@@ -0,0 +1,7 @@
+public class MovieQuote
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Whenever feasible, one should always try to eat the rude. Dr. Hannibal Lecter");
+    }
+}
